a76c8ee93111dfa00e7227461301fae0cc96a47d
Денис Пиневич <Den2904@yandex.ru>
2023-08-08 12:07:21 +0300

    После третьего ревью попытка 47

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,8 +1,8 @@
-from django_filters.rest_framework import FilterSet, filters
+from django_filters.rest_framework import filters
 from recipes.models import Recipe, Tag
 
 
-class RecipeFilter(FilterSet):
+class RecipeFilter(filters.FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
